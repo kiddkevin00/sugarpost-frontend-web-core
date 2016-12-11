0c9a501f427c0db24906472f12de8d82ef23938b
Marcus Hsu <Marcus.Hsu@adp.com>
2016-12-10 22:45:32 -0500

    [System] Bump the version to 1.6.0.

--- src/lib/client/src/app/login/components/LoginApp.js
+++ src/lib/client/src/app/login/components/LoginApp.js
@@ -47,7 +47,7 @@ class LoginApp extends BaseComponent {
                   <p className="text-muted">
                     <a href="mailto:info@mysugarpost.com">Development Support</a>
                   </p>
-                  <p>v1.5.3</p>
+                  <p>v1.6.0</p>
                 </div>
               </div>
             </div>
