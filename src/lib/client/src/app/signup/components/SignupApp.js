94dd1b8cb7c216d4cfa068b1068641558dbc4bc8
Marcus Hsu <Marcus.Hsu@adp.com>
2016-12-15 00:01:15 -0500

    [System] Bump the version to 1.6.8

--- src/lib/client/src/app/signup/components/SignupApp.js
+++ src/lib/client/src/app/signup/components/SignupApp.js
@@ -47,7 +47,7 @@ class SignupApp extends BaseComponent {
                   <p className="text-muted">
                     <a href="mailto:info@mysugarpost.com">Development Support</a>
                   </p>
-                  <p>v1.6.7</p>
+                  <p>v1.6.8</p>
                 </div>
               </div>
             </div>
