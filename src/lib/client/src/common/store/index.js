c5cac63a95830b61739b18f8f8a647dc6b080633
Marcus Hsu <Marcus.Hsu@adp.com>
2017-08-06 13:34:11 -0400

    Add "redux-logger" middleware for Redux store

--- src/lib/client/src/common/store/index.js
+++ src/lib/client/src/common/store/index.js
@@ -1,11 +1,13 @@
 import rootReducer from '../reducer/';
 import thunkMiddleware from 'redux-thunk';
+import logger from 'redux-logger';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 
 function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(thunkMiddleware),
+    applyMiddleware(logger),
     typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ?
       window.devToolsExtension() : (f) => f,
   ));
